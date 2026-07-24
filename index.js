bd687879b351a150a07d1af6f4b97216cd223ee6
Anish sapkota <anishsapkota265@gmail.com>
2026-07-24 12:31:11 +0545

    server url and method added

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ const httpServer = http.createServer((req,res)=>{
     // every incomming data from header, cookie, body, query, params, file or files is collected/stored by req
     // manipulate the request
     // respond to rhe client
-     res.end(" Hello there")
+    const url = req.url
+    const method = req.method
+    console.log({url,method})
+
+    if (url==="/" && method ==="GET"){
+        console.log(res.end("Home Page"))
+
+    }else if(url === "/about-us" && method ==="GET"){
+        console.log(res.end("About-us"))
+    }else {
+        console.log(res.end("Error Page................"))
+    }
 })
+
 
 const HOST = "localhost"       // Url => service, hostname or url , localhost= 127.0.0.1 ,  ::1
 const PORT = 9005     // 0-2^16-1 ~~ 65535
