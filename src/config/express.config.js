96a121365f6cfd237eaefadec5a6f9f298bf0e45
Anish sapkota <anishsapkota265@gmail.com>
2026-07-29 11:40:47 +0545

    404 not found catch using middleware

--- src/config/express.config.js
+++ src/config/express.config.js
@@ -4,13 +4,22 @@ const router = require("./router.config")
 const app = express()
 
 // Parser
+
+// router mounting
+app.use(router)
+
 app.use((req,res,next)=>{
+    res.status(404).json({
+        error : null,
+        message :"route not found",
+        status : "NOT_FOUND"
+
+
+    })
     console.log("I am here")
     next()
 
 })
-// router mounting
-app.use(router)
 
 
 
