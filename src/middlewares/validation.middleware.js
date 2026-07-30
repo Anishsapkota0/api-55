25b003bfd8f788e920316246bb673a9ceb98229f
Anish sapkota <anishsapkota265@gmail.com>
2026-07-30 23:40:16 +0545

    exception handelling start

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -2,11 +2,22 @@ const bodyValidator = (schema)=>{
     return async (req,res,next)=>{
         try{
       const data = req.body;
-      await schema.validateAsync(data, {abortEarly: false})
+      if (!data){
+        throw {code : 422 , message : " Data required", status : "Empty_Payload"}
+      }
+      console.log(data)
+    //   console.log(data)
+     let result = await schema.validateAsync(data, {abortEarly: false})
+    //  console.log(result)
+
+
     }catch(exception){
+        console.log(exception)
         //handle
+        next(exception)
 
     }
+    
 }
 
 }
